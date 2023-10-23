96cd564468315458e11f981943727ac3bc9ae854
YongSoo Cho <soo_0201@naver.com>
2023-10-23 15:41:42 +0900

    실전 예제 5까지

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.*;
+
+@Entity
+public class Delivery extends BaseEntity {
+
+    @Id @GeneratedValue
+    @Column(name = "DELIVERY_ID")
+    private Long id;
+    private String city;
+    private String street;
+    private String zipcode;
+    private DeliveryStatus status;
+
+    @OneToOne(mappedBy = "delivery", fetch =  LAZY)
+    private Order order;
+
+}
